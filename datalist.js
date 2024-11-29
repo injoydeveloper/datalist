63d1cd1706ff5e70cbc9110fd88987fef9859997
T. Thor Leach <tleach@injoy.local>
2024-11-29 14:08:38 -0700

    Update datalist.js

--- datalist.js
+++ datalist.js
@@ -1,8 +1,8 @@
 
 (function() {
-    if (!document.querySelector("#datalist-polyfill-style")) {
+    if (!document.querySelector("#datalist-style")) {
         let style = document.createElement("style");
-        style.id = "datalist-polyfill-style";
+        style.id = "datalist-style";
         style.textContent = `
             fieldset {
                 border: 1px solid black;
@@ -17,7 +17,7 @@
 
             input[list] {
                 padding: 5px;
-                height: 35px;
+                /*height: 35px;*/
                 width: 1000px;
                 border: 1px solid black;
                 outline: none;
@@ -75,82 +75,108 @@
         }
     }
 
-    let datalists = document.querySelectorAll('datalist');
-    for (let datalist of datalists) {
-        datalist.setAttribute("role",'listbox'); // style variable //
-    }
-
-    let inputs = document.querySelectorAll('input[list]');
-    for (let input of inputs) {
+    function dataListStyle() {
         
-        if (input.getAttribute('data-list')) {
-            // this input has already been enhanced
-            return;
+        let datalists = document.querySelectorAll('datalist');
+        for (let datalist of datalists) {
+            datalist.setAttribute("role",'listbox'); // style variable //
         }
 
-        // find corresponding datalist
-        let listID = input.getAttribute('list');
-        let list = document.getElementById(listID);
-        
-        input.setAttribute("data-list", listID);
-        input.setAttribute("list", "");
-
-        input.setAttribute("autocomplete", "off");
-        input.setAttribute("role", "combobox");
-        // if the z-index of the input is less than the z-index of the datalist, the datalist will be hidden behind the input
-        // we also need a mutation observer to watch for new options being added to the datalist or new datalist inputs being added to the page
-        // should we make a fake datalist element that gets added to the page in case multiple inputs are using the same datalist?
-        // the fieldset element is used to group the input and datalist together. What if there's no fieldset? the input needs to be left justified, so we have to use a legend element instead of a label element
-
-        let options = list.querySelectorAll('option');
-        let currentFocus = -1;
-
-        input.addEventListener("focus", function() {
+        let inputs = document.querySelectorAll('input[list]');
+        for (let input of inputs) {
             
-            list.style.display = 'block';  // style variable //
-            input.style.borderRadius = "5px 5px 0 0"; // style variable //
+            if (!input.getAttribute('data-list')) {
+     
+                // find corresponding datalist
+                let listID = input.getAttribute('list');
+                let list = document.getElementById(listID);
+                
+                input.setAttribute("data-list", listID);
+                input.setAttribute("list", "");
 
-            for (let option of options) {
-                // if (option.value != option.innerHTML) {
-                //     console.log(option);
-                //     // option.innerHTML = option.value;
-                // }
-                option.addEventListener("click", function() {
-                    input.value = option.value;
-                    list.style.display = 'none'; // style variable //
-                    input.style.borderRadius = "5px"; // style variable //
+                input.setAttribute("autocomplete", "off");
+                input.setAttribute("role", "combobox");
+                // if the z-index of the input is less than the z-index of the datalist, the datalist will be hidden behind the input
+                // we also need a mutation observer to watch for new options being added to the datalist or new datalist inputs being added to the page
+                // adobt the datalist node and move it in the page as it's being used so that it is immediately after the input.
+                // the fieldset element is used to group the input and datalist together. What if there's no fieldset? the input needs to be left justified, so we have to use a legend element instead of a label element
+
+                let options = list.querySelectorAll('option');
+                let currentFocus = -1;
+
+                input.addEventListener("focus", function() {
+                    
+                    list.style.display = 'block';  // style variable //
+                    input.style.borderRadius = "5px 5px 0 0"; // style variable //
+                    let inputOffset = getOffset(input);
+                    let inputHeight = input.offsetHeight;
+
+                    let inputzIndex = window.getComputedStyle(input).getPropertyValue('z-index');
+                    list.style.zIndex = inputzIndex;
+                    list.style.top = inputOffset.top + inputHeight + "px";
+                    list.style.left = inputOffset.left + "px";
+                    
+
+                    for (let option of options) {
+                        option.addEventListener("click", function() {
+                            input.value = option.value;
+                            input.dispatchEvent(new Event("input"))
+                            list.style.display = 'none'; // style variable //
+                            input.style.borderRadius = "5px"; // style variable //
+                        });
+                    }
+                });
+
+                input.addEventListener("input", function() {
+                    currentFocus = -1;
+                    let text = input.value.toUpperCase();
+                    for (let option of options) {
+                        if (option.value.toUpperCase().indexOf(text) > -1) {
+                            option.style.display = "block";
+                        } else {
+                            option.style.display = "none";
+                        }
+                    }
+                });
+
+                input.addEventListener("keydown", function(e) {
+                    if (e.keyCode == 40) {
+                        currentFocus++;
+                        addActive(options);
+                    } else if (e.keyCode == 38) {
+                        currentFocus--;
+                        addActive(options);
+                    } else if (e.keyCode == 13) {
+                        e.preventDefault();
+                        if (currentFocus > -1) {
+                            /* and simulate a click on the "active" item: */
+                            if (options) {
+                                options[currentFocus].click();
+                            }
+                        }
+                    }
                 });
             }
-        });
-
-        input.addEventListener("input", function() {
-            currentFocus = -1;
-            let text = input.value.toUpperCase();
-            for (let option of options) {
-                if (option.value.toUpperCase().indexOf(text) > -1) {
-                    option.style.display = "block";
-                } else {
-                    option.style.display = "none";
-                }
-            }
-        });
-
-        input.addEventListener("keydown", function(e) {
-            if (e.keyCode == 40) {
-                currentFocus++;
-                addActive(options);
-            } else if (e.keyCode == 38) {
-                currentFocus--;
-                addActive(options);
-            } else if (e.keyCode == 13) {
-                e.preventDefault();
-                if (currentFocus > -1) {
-                    /* and simulate a click on the "active" item: */
-                    if (options) {
-                        options[currentFocus].click();
-                    }
-                }
-            }
-        });
+        }
     }
+
+    function getOffset(el) {
+        const rect = el.getBoundingClientRect();
+        return {
+          left: rect.left + window.scrollX,
+          top: rect.top + window.scrollY
+        };
+      }
+
+    const targetNode = document.getElementsByTagName('body')[0];
+    const observer = new MutationObserver(function(mutationsList, observer) {
+        observer.disconnect();
+        dataListStyle();
+        observer.observe(targetNode, {childList:true,subtree: true});
+    });
+    const config = {childList:true,subtree: true};
+
+    observer.observe(targetNode, config);
+
+
 })();
